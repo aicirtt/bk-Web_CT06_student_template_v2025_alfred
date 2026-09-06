63f959a8fac2452039e45949a1ac06e64d4c43ed
aicirtt <158793466+aicirtt@users.noreply.github.com>
2026-09-06 14:28:34 +0000

    Sep 6, 2026, 2:28 PM

--- L08_list_displayText.js
+++ L08_list_displayText.js
@@ -98,25 +98,25 @@
 /////////////////////////////////////////////////////////
 // Task 4: Endless Scrolling
 
-let storyText = [
-  "A long time ago in a galaxy far,",
-  "far away...",
-  "",
-  "EPISODE I",
-  "THE BEGINNING",
-  "",
-  "It is a period of learning...",
-  "Students have begun their journey",
-  "into the world of p5.js.",
-  "With newfound powers, they",
-  "create amazing visuals and",
-  "animations...",
-  "",
-  "May the Code be with you!"
-];
+// let storyText = [
+//   "A long time ago in a galaxy far,",
+//   "far away...",
+//   "",
+//   "EPISODE I",
+//   "THE BEGINNING",
+//   "",
+//   "It is a period of learning...",
+//   "Students have begun their journey",
+//   "into the world of p5.js.",
+//   "With newfound powers, they",
+//   "create amazing visuals and",
+//   "animations...",
+//   "",
+//   "May the Code be with you!"
+// ];
 
-let yPos = 400;
-let lineGap = 35;
+// let yPos = 400;
+// let lineGap = 35;
 
 // function setup() {
 //   createCanvas(400, 400);
@@ -152,7 +152,67 @@ let lineGap = 35;
 
 /////////////////////////////////////////////////////////
 // Challenge 1: Background music
+let storyText = [
+  "A long time ago in a galaxy far,",
+  "far away...",
+  "",
+  "EPISODE I",
+  "THE BEGINNING",
+  "",
+  "It is a period of learning...",
+  "Students have begun their journey",
+  "into the world of p5.js.",
+  "With newfound powers, they",
+  "create amazing visuals and",
+  "animations...",
+  "",
+  "May the Code be with you!"
+];
 
+let yPos = 400;
+let lineGap = 35;
+
+let bgMusic;
+
+function preload() {
+  bgMusic = loadSound("star_wars_theme_8_bit.mp3");
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+
+  fill(255, 255, 0);
+  textSize(24);
+  textAlign(CENTER, CENTER);
+
+  for (let i = 0; i < storyText.length; i++) {
+    text(
+      storyText[i],
+      width / 2,
+      yPos + i * lineGap
+    );
+  }
+
+  // Move text upwards
+  yPos = yPos - 0.6;
+
+  // Reset when the whole story goes off screen
+  if (yPos < -storyText.length * lineGap) {
+    yPos = height;
+  }
+}
+
+function mousePressed() {
+  userStartAudio();
+
+  if (!bgMusic.isPlaying()) {
+    bgMusic.loop();
+  }
+}
 /////////////////////////////////////////////////////////
 // Challenge 2: Click to start
 
