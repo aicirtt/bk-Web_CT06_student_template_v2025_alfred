5c75bad2cb10d69566d1a240040be83693b50234
aicirtt <158793466+aicirtt@users.noreply.github.com>
2026-09-06 14:27:23 +0000

    Sep 6, 2026, 2:27 PM

--- L08_list_displayText.js
+++ L08_list_displayText.js
@@ -153,49 +153,49 @@ let lineGap = 35;
 /////////////////////////////////////////////////////////
 // Challenge 1: Background music
 
-let bgMusic;
+// let bgMusic;
 
-function preload() {
-  bgMusic = loadSound("assets/star_wars_theme_8_bit.mp3");
-}
+// function preload() {
+//   bgMusic = loadSound("assets/star_wars_theme_8_bit.mp3");
+// }
 
-function setup() {
-  createCanvas(400, 400);
-}
-function draw() {
-  background(0);
-  bgMusic.play();
-  fill(255, 255, 0);
-  textSize(24);
-  textAlign(CENTER, CENTER);
+// function setup() {
+//   createCanvas(400, 400);
+// }
+// function draw() {
+//   background(0);
+//   bgMusic.play();
+//   fill(255, 255, 0);
+//   textSize(24);
+//   textAlign(CENTER, CENTER);
 
-  // Display every line in the array
-  for (let i = 0; i < storyText.length; i++) {
-    let sentence = storyText[i];
+//   // Display every line in the array
+//   for (let i = 0; i < storyText.length; i++) {
+//     let sentence = storyText[i];
 
-    text(
-      sentence,
-      width / 2,
-      yPos + i * lineGap
-    );
-  }
+//     text(
+//       sentence,
+//       width / 2,
+//       yPos + i * lineGap
+//     );
+//   }
 
-  // Move the text upwards
-  yPos = yPos - 0.6;
+//   // Move the text upwards
+//   yPos = yPos - 0.6;
 
-  // If the whole story has moved off screen,
-  // move it back to the bottom
-  if (yPos < -storyText.length * lineGap) {
-    yPos = height;
-  }
-}
-function mousePressed() {
-  userStartAudio();
+//   // If the whole story has moved off screen,
+//   // move it back to the bottom
+//   if (yPos < -storyText.length * lineGap) {
+//     yPos = height;
+//   }
+// }
+// function mousePressed() {
+//   userStartAudio();
 
-  if (!bgMusic.isPlaying()) {
-    bgMusic.loop();
-  }
-}
+//   if (!bgMusic.isPlaying()) {
+//     bgMusic.loop();
+//   }
+// }
 /////////////////////////////////////////////////////////
 // Challenge 2: Click to start
 
