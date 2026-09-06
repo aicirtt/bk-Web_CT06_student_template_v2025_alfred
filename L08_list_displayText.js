cc112388e6977b7d91a6ae064a7885bdf47fda70
aicirtt <158793466+aicirtt@users.noreply.github.com>
2026-09-06 14:24:15 +0000

    Sep 6, 2026, 2:24 PM

--- L08_list_displayText.js
+++ L08_list_displayText.js
@@ -122,33 +122,33 @@ let lineGap = 35;
 //   createCanvas(400, 400);
 // }
 
-function draw() {
-  background(0);
+// function draw() {
+//   background(0);
 
-  fill(255, 255, 0);
-  textSize(24);
-  textAlign(CENTER, CENTER);
+//   fill(255, 255, 0);
+//   textSize(24);
+//   textAlign(CENTER, CENTER);
 
-  // Display every line in the array
-  for (let i = 0; i < storyText.length; i++) {
-    let sentence = storyText[i];
+//   // Display every line in the array
+//   for (let i = 0; i < storyText.length; i++) {
+//     let sentence = storyText[i];
 
-    text(
-      sentence,
-      width / 2,
-      yPos + i * lineGap
-    );
-  }
+//     text(
+//       sentence,
+//       width / 2,
+//       yPos + i * lineGap
+//     );
+//   }
 
-  // Move the text upwards
-  yPos = yPos - 0.6;
+//   // Move the text upwards
+//   yPos = yPos - 0.6;
 
-  // If the whole story has moved off screen,
-  // move it back to the bottom
-  if (yPos < -storyText.length * lineGap) {
-    yPos = height;
-  }
-}
+//   // If the whole story has moved off screen,
+//   // move it back to the bottom
+//   if (yPos < -storyText.length * lineGap) {
+//     yPos = height;
+//   }
+// }
 
 /////////////////////////////////////////////////////////
 // Challenge 1: Background music
@@ -162,7 +162,83 @@ function preload() {
 function setup() {
   createCanvas(400, 400);
 }
+// Challenge 2: Click to start
 
+let storyText = [
+  "A long time ago in a galaxy far,",
+  "far away...",
+  "",
+  "EPISODE I",
+  "THE BEGINNING",
+  "",
+  "It is a period of learning...",
+  "Students have begun their journey",
+  "into the world of p5.js.",
+  "With newfound powers, they",
+  "create amazing visuals and",
+  "animations...",
+  "",
+  "May the Code be with you!"
+];
+
+let yPos = 400;
+let lineGap = 35;
+
+let started = false;
+let bgMusic;
+
+function preload() {
+  bgMusic = loadSound("star_wars_theme_8_bit.mp3");
+}
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+
+  if (started == false) {
+    // Start screen
+    fill(255);
+    textSize(24);
+    textAlign(CENTER, CENTER);
+    text("Click to start the show", width / 2, height / 2);
+
+  } else {
+    // Show the scrolling story
+    fill(255, 255, 0);
+    textSize(24);
+    textAlign(CENTER, CENTER);
+
+    for (let i = 0; i < storyText.length; i++) {
+      text(
+        storyText[i],
+        width / 2,
+        yPos + i * lineGap
+      );
+    }
+
+    // Scroll upwards
+    yPos = yPos - 0.6;
+
+    // Reset when story goes off screen
+    if (yPos < -storyText.length * lineGap) {
+      yPos = height;
+    }
+  }
+}
+
+function mousePressed() {
+  if (started == false) {
+    started = true;
+
+    userStartAudio();
+    bgMusic.loop();
+  }
+}
+
+/////////////////////////////////////////////////////////
 function mousePressed() {
   userStartAudio();
 
