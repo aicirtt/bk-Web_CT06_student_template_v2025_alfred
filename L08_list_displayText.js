f8892e840e5f28e3fbf3645b195c279183d8bec4
aicirtt <158793466+aicirtt@users.noreply.github.com>
2026-09-06 13:56:46 +0000

    Sep 6, 2026, 1:56 PM

--- L08_list_displayText.js
+++ L08_list_displayText.js
@@ -39,12 +39,33 @@ function draw() {
 
 /////////////////////////////////////////////////////////
 // Task 1: Self Introduction
+function setup() {
+  createCanvas(400, 400);
 
+  textSize(16);
+
+  text("My name is Tricia", 50, 50);
+  text("I am ___ years old", 50, 100);
+  text("My favourite activity is coding", 50, 150);
+}
 
 
 /////////////////////////////////////////////////////////
 // Task 2:
+let foods = ["Pizza", "Burger", "Sushi", "Tacos", "Pasta"];
 
+function setup() {
+  createCanvas(400, 400);
+
+  textSize(16);
+  textAlign(LEFT);
+
+  text("My Favourite Foods:", 50, 50);
+
+  for (let i = 0; i < foods.length; i++) {
+    text((i + 1) + ". " + foods[i], 50, 80 + i * 25);
+  }
+}
 
 
 /////////////////////////////////////////////////////////
