263ab9c50bfe895ac41cc25344d6973e45459cee
aicirtt <158793466+aicirtt@users.noreply.github.com>
2026-09-07 10:27:26 +0000

    Sep 7, 2026, 10:27 AM

--- L08_list_displayText.js
+++ L08_list_displayText.js
@@ -58,10 +58,12 @@ function draw(){
   fill(255);
 
   text("My favourite foods: ",50,50);
-  
+
   for(let i = 0 ; i <foods.length; i++) {
     text((i+1)+". " + foods[i] , 50, 80+i*25);
   }
+
+  
 }
 
 
