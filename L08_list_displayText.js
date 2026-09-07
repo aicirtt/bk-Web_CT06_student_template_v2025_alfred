2d07a9473f06d8c5aa957ad844d26cb65cbcbadd
aicirtt <158793466+aicirtt@users.noreply.github.com>
2026-09-07 10:34:02 +0000

    Sep 7, 2026, 10:34 AM

--- L08_list_displayText.js
+++ L08_list_displayText.js
@@ -77,6 +77,7 @@ let storyText = [
   "It is a period of learning...",
   "Student have begun their journey",
   "into the world of p5.js." ,
+  "With newfound powers, they"
 ]
 // Challenge 1: Background music
 // Challenge 2: Click to start
