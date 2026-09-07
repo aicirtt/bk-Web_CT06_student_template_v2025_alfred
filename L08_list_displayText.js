b21b02b33898a665234a5d5d04b787cbf572840e
aicirtt <158793466+aicirtt@users.noreply.github.com>
2026-09-07 10:41:32 +0000

    Sep 7, 2026, 10:41 AM

--- L08_list_displayText.js
+++ L08_list_displayText.js
@@ -97,7 +97,7 @@ function draw(){
   //display each line in the array 
   for(let i = 0 ; i<storyText.length ; i++){
     let sentence = storyText[i];
-    text(sentence, width/2, );
+    text(sentence, width/2, 80+i*25);
   }
 
 }
