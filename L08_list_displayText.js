cdd28cfc11a80a0cbb4277620d54183224d0434c
aicirtt <158793466+aicirtt@users.noreply.github.com>
2026-09-07 10:46:47 +0000

    Sep 7, 2026, 10:46 AM

--- L08_list_displayText.js
+++ L08_list_displayText.js
@@ -105,7 +105,9 @@ function draw(){
 
   yPos= yPos - 0.6;
 
-  
+  //if story is gone , we want to move it back to the bottom
+  if (yPos < storyText.length*lineGap){}
+
 }
 // Challenge 1: Background music
 // Challenge 2: Click to start
